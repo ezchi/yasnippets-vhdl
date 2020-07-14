bc041c209c1f436e569583d22831c18d44ae5e7b
Enze Chi <enze.chi@gmail.com>
2020-07-14 14:18:22 +1000

    Add autoload

---
+++ yasnippets-vhdl.el
@@ -0,0 +1,32 @@
+;;; yasnippet-vhdl.el --- Yasnippets for Vhdl
+
+;; Copyright (C) 2020 Enze Chi
+
+;; Author: Enze Chi
+;; Version: 0.0.1
+;; URL: https://github.com/ezchi/yasnippets-vhdl.git
+;; Package-Requires: ((yasnippet "0.8.0"))
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'yasnippet)
+
+(defvar yasnippets-vhdl-root
+  (file-name-directory (or load-file-name (buffer-file-name))))
+
+;;;###autoload
+(defun yasnippets-vhdl-initialize ()
+  (let ((snip-dir (expand-file-name "snippets" yasnippets-vhdl-root)))
+    (when (boundp 'yas-snippet-dirs)
+      (add-to-list 'yas-snippet-dirs snip-dir t))
+    (yas-load-directory snip-dir)))
+
+;;;###autoload
+(eval-after-load 'yasnippet
+  '(yasnippets-vhdl-initialize))
+
+(provide 'yasnippets-vhdl)
+
+;;; yasnippets-vhdl.el ends here
